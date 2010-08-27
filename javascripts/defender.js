dae8d730504f254f18446bb6f34fa60c9df5b163
Cameron Daigle <cameron@hashrocket.com>
2010-08-27 17:34:45 -0400

    Bad guys shoot now

--- javascripts/defender.js
+++ javascripts/defender.js
@@ -4,28 +4,33 @@ $(function() {
   var settings = {
     canvas_width: canvas.clientWidth,
     canvas_height: canvas.clientHeight,
-    shot_interval : 400,
-    shot_fired : false,
     spawn_baddie : true,
     spawn_rate : 2000,
     default_baddie_speed : 3,
-    default_shot_speed : 30
+    default_shot_speed : 30,
+    pause: false
   };
   var hr = {
     x : 20,
     y : 20,
     speed : 8,
-    shots : [],
     width : 74,
     height: 68,
-    dead : false
+    dead : false,
+    shot_interval : 400,
+    shot_fired : false,
+    shots : []
   };
   var baddie = {
     x : settings.canvas_width + 20,
     y : 0,
     speed : settings.default_baddie_speed,
     width: 20,
-    height: 20
+    height: 20,
+    color: "#00ff00",
+    shot_interval : 1500,
+    shot_fired : true,
+    shot_initial_delay : 500
   };
   var shot = {
     x : 0,
@@ -33,27 +38,29 @@ $(function() {
     speed : settings.default_shot_speed
   };
   var baddies = [];
+  var baddie_shots = [];
   var key = [];
   this.onkeydown = this.onkeyup = function(e) {
     key[e.which] = (e.type == 'keydown');
+    if((e.type == 'keydown') && (e.keyCode == 80)) {
+      settings.pause = !settings.pause;
+    }
   };
   setInterval(function() {
+    if(settings.pause) { return; }
     move_hr();
-    if (key[32] && !hr.dead) {
-      if(!settings.shot_fired) {
-        var newShot = $.extend({}, shot);
-        newShot.x = hr.x + 74;
-        newShot.y = hr.y + 34;
-        hr.shots.push(newShot);
-        
-        settings.shot_fired = true;
-        setTimeout(function() { settings.shot_fired = false;}, settings.shot_interval);
+
+    if(!hr.shot_fired) {
+      if (key[32] && !hr.dead) {
+        hr.shoot();
       }
     }
     if(settings.spawn_baddie) {
       if(!hr.dead) {
         var newBaddie = $.extend({}, baddie);
         baddie.y = Math.random() * settings.canvas_height;
+        baddie.shot_interval = Math.random() * 4000 + 1000;
+        baddie.shot_initial_delay = Math.random() * 1500 + 1000;
         baddies.push(newBaddie);
         settings.spawn_baddie = false;
         setTimeout(function() { settings.spawn_baddie = true; }, settings.spawn_rate);
@@ -80,9 +87,16 @@ $(function() {
       } else {
         if(!hr.dead) {
           b.x -= b.speed;
+          baddie_shoot(b);
+          var j;
         }
         draw_baddie(b);
       }
+    }
+    for(j=0; j<baddie_shots.length; j++) {
+      var bs = baddie_shots[j];
+      bs.x -= bs.speed;
+      draw_baddie_shot(bs);
     }
     draw_hr();
   }, 15);
@@ -96,10 +110,10 @@ $(function() {
     }
     if(hr.y > 0) {
       hr.y -= key[38]?hr.speed:0;
-    };
+    }
     if(hr.y < (settings.canvas_height - 68)) {
       hr.y += key[40]?hr.speed:0;
-    };
+    }
   };
   var check_collisions = function() {
     if(hr.dead) {return;}
@@ -115,11 +129,45 @@ $(function() {
       for(j=0; j<hr.shots.length; j++) {
         var s = hr.shots[j];
         if((s.y > b.y) && (s.y < (b.y + b.height)) && (s.x > b.x)) {
-          baddies.splice(i, 1);
+          kill_baddie(i);
           hr.shots.splice(j, 1);
         }
-      };
+      }
     };
+    for(i=0; i<baddie_shots.length; i++) {
+      s = baddie_shots[i];
+      if((s.x < (hr.x + hr.width)) && (s.x > hr.x)) {
+        if((s.y < (hr.y + hr.height)) && (s.y > hr.y)) {
+          kill_hr();
+          baddie_shots.splice(i, 1);
+        }
+      }
+    }
+  };
+  hr.shoot = function() {
+    var hr = this;
+    var newShot = $.extend({}, shot);
+    newShot.x = hr.x + 74;
+    newShot.y = hr.y + 34;
+    hr.shots.push(newShot);
+
+    hr.shot_fired = true;
+    setTimeout(function() { hr.shot_fired = false;}, hr.shot_interval);
+  };
+  var baddie_shoot = function(b) {
+    if(b.shot_initial_delay) {
+      setTimeout(function() { b.shot_fired = false; }, b.shot_initial_delay);
+      b.shot_initial_delay = false;
+    }
+    if(!b.shot_fired) {
+      var newShot = $.extend({}, shot);
+      newShot.x = b.x;
+      newShot.y = b.y + (b.height/2);
+      newShot.speed = 8;
+      baddie_shots.push(newShot);
+      b.shot_fired = true;
+      setTimeout(function() { b.shot_fired = false;}, b.shot_interval);
+    }
   };
   var kill_hr = function() {
     hr.dead = true;
@@ -141,6 +189,14 @@ $(function() {
       }, (100*i)+50);
     }
   };
+  var kill_baddie = function(i) {
+    var b = baddies[i];
+    b.color = "#cc0000";
+    b.speed = 0;
+    setTimeout(function() {
+      baddies.splice(i, 1);
+    }, 50);
+  };
   var draw_bg = function() {
     ctx.beginPath();
     ctx.fillStyle = "#000000";
@@ -153,9 +209,15 @@ $(function() {
     ctx.moveTo(x, y);
     ctx.fillRect(x, y, 10, 2);
   };
+  var draw_baddie_shot = function(s) {
+    ctx.beginPath();
+    ctx.fillStyle = "#aaffff";
+    ctx.moveTo(s.x, s.y);
+    ctx.fillRect(s.x, s.y, 4, 4);
+  };
   var draw_baddie = function(b) {
     ctx.beginPath();
-    ctx.fillStyle = "#00ffff";
+    ctx.fillStyle = b.color;
     ctx.moveTo(b.x, b.y);
     ctx.fillRect(b.x, b.y, b.width, b.height);
   };
